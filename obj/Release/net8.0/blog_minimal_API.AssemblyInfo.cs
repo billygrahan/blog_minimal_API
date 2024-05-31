4a52569a6a5e660592e292054263a0681a8c302d
billygrahan <billygrahan@alu.ufc.br>
2024-05-31 14:13:31 -0300

    implementado todos os endpoints mas ainda precisando de altentificação de token

--- obj/Release/net8.0/blog_minimal_API.AssemblyInfo.cs
+++ obj/Release/net8.0/blog_minimal_API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("blog_minimal_API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35f3c7ce1b6a1e2a1d434540d77aca2a91603322")]
 [assembly: System.Reflection.AssemblyProductAttribute("blog_minimal_API")]
